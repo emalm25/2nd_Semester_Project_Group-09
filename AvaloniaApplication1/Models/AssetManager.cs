d8a1b9816c663045bf52fb5c64beb48e417d4a22
Emilia <pezxarl@gmail.com>
2026-03-03 20:52:39 +0100

    asset manager.0

--- AvaloniaApplication1/Models/AssetManager.cs
+++ AvaloniaApplication1/Models/AssetManager.cs
@@ -1,48 +1,100 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 public class HeatingGrid
 {
-    private string Name;
-    private string ImagePath;
+    public string Name {get; private set;}
 
-    public HeatingGrid(string Name, string ImagePath)
+    public HeatingGrid(string Name)
     {
         this.Name = Name;
-        this.ImagePath = ImagePath;
     }
 }
 
 public class ProductionUnit
 {
-    private string Name;
-    private string ImagePath;
-    private double ProducedHeat;
-    private double ConsumptionEnegry;
-    private decimal ProductionCosts;
+    public string ShortName {get; private set;}
+    public string Name {get; private set;}
+    public double MaxHeat {get; private set;}
+    public decimal ProductionCosts {get; private set;}
+    public double? CO2Emissions {get; private set;}
 
-    private double CO2Emissions;
-
-    public ProductionUnit(string Name, string ImagePath, double ProducedHeat,double ConsumptionEnegry, decimal ProductionCosts,double CO2Emissions )
+    public ProductionUnit(string ShortName, string Name, double MaxHeat, decimal ProductionCosts,double CO2Emissions )
     {
+        this.ShortName = ShortName;
         this.Name = Name;
-        this.ImagePath = ImagePath;
-        this.ProducedHeat = ProducedHeat;
-        this.ConsumptionEnegry = ConsumptionEnegry;
+        this.MaxHeat = MaxHeat;
         this.ProductionCosts = ProductionCosts;
         this.CO2Emissions = CO2Emissions;
     }
 }
 
-public class Machines
-{ 
-    List<string> names = new List<string> {"Gas boiler 1", "Gas boiler 2", "Gas boiler 3", "Oil boiler 1", "Gas motor 1", "Electric boiler 1" }; 
+    public class GasBoilersInfo : ProductionUnit
+    {
+        public double GasConsumption { get; private set;}
 
-}
+        public GasBoilerInfo(string ShortName, string Name, double MaxHeat, decimal ProductionCosts, double CO2Emissions, double GasConsumption)
+        : base(ShortName, Name, MaxHeat, ProductionCosts, CO2Emissions)
+        {
+            GasConsumption = GasConsumption;
+        }
+       
+    }
 
 
+    public class OilBoilerInfo : ProductionUnit
+    {
+        public double OilConsumption { get; private set;}
+
+        public OilBoilerInfo(string ShortName, string Name, double MaxHeat, decimal ProductionCosts, double CO2Emissions, double OilConsumption)
+        : base(ShortName, Name, MaxHeat, ProductionCosts, CO2Emissions)
+        {
+            this.OilConsumption = OilConsumption;
+        }
 
 
+    }
+
+      public class GasMotorInfo : ProductionUnit
+    {
+        public double Gas2Consumption { get; private set;}
+        public double MaxElectricity { get; private set;}
+
+        public GasMotorInfo(string ShortName, string Name, double MaxHeat, double MaxElectricity, decimal ProductionCosts, double CO2Emissions, double Gas2Consumption)
+        : base(ShortName, Name, MaxHeat, ProductionCosts, CO2Emissions)
+        {
+            this.Gas2Consumption = Gas2Consumption;
+            this.MaxElectricity = MaxElectricity;
+        }
+
+
+    }
+
+    public class ElectricBoilerInfo : ProductionUnit
+    {
+        public double MaxElectricity { get; private set; }
+        public ElectricBoilerInfo(string ShortName, string Name, double MaxHeat, double MaxElectricity, decimal ProductoinCost )
+        : base(ShortName, Name, MaxHeat, ProductionCosts, null)
+        {
+            this.MaxElectricity = MaxElectricity;
+        }
+    }
+        public class AssetManager
+        {
+    
+           public List<ProductionUnit> Units { get; } = new List<ProductionUnit>
+        {
+            new GasBoilerInfo("GB1", "Gas Boiler 1", 3.0 , 510, 132, 1.05),
+            new GasBoilerInfo("GB2", "Gas Boiler 2", 2.0 , 540, 134, 1.08),
+            new GasBoilerInfo("GB3", "Gas Boiler 3", 4.0 , 580, 136, 1.09),
+            new OilBoilerInfo("OB1", "Oil Boiler 1", 6.0, 690, 147, 1.18),
+            new GasMotorInfo("GM1", "Gas Motor 1", 5.3, 3.9, 975, 227, 1.82),
+            new ElectricBoilerInfo("EB1", "Electric Boiler", 6.0, -6.0, 15)
+        };
+
+
+        }
 
     
 

224401d9b82a88dbaca6da7098795a0fefb98c48
Noah-SDU <nogau25@student.sdu.dk>
2026-03-10 16:07:15 +0100

    umm fixed folder stuff i guess idk

---
+++ HeatProductionOptimization/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HeatProductionOptimization.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+    public string AssetUnitNames { get; }
+
+    
+
+
+}
+
---
+++ HeatProductionOptimization/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace HeatProductionOptimization.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}

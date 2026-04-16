3afad8e5ae7de99362d06d1b2a7d60f33672096e
Valeria <varon25@student.sdu.dk>
2026-04-16 13:06:47 +0200

    fix deteils
    background

--- HeatProductionOptimization/obj/Debug/net10.0/HeatProductionOptimization.AssemblyInfo.cs
+++ HeatProductionOptimization/obj/Debug/net10.0/HeatProductionOptimization.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatProductionOptimization")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea19757a0fe3baba28eac32d0ba22947c5ebb834")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43c06b31bf769ef548db665f22737e4593d1d7f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatProductionOptimization")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatProductionOptimization")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

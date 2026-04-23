c8bf72c79eb48119394a42a997d3995beaed845b
Emilia <pezxarl@gmail.com>
2026-04-23 17:44:13 +0200

    New units adding

--- HeatProductionOptimization/Models/AssetManager.cs
+++ HeatProductionOptimization/Models/AssetManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
+using HeatProductionOptimization.Services;
 
 public class HeatingGrid
 {
@@ -16,19 +17,18 @@ public class HeatingGrid
 
 public class ProductionUnit
 {
-    public string ShortName {get; private set;}
-    public string Name {get; private set;}
+    public string ShortName {get; set;}
+    public string Name {get; set;}
     public bool IsAvailable {get; set;}
-    public double MaxHeat {get; private set;}
-    public decimal ProductionCosts {get; private set;}
-    public double? CO2Emissions {get; private set;}
+    public double MaxHeat {get; set;}
+    public decimal ProductionCosts {get; set;}
+    public double? CO2Emissions {get; set;}
     
-    // Virtual properties for specific unit types
-    public virtual double? GasConsumption => null;
-    public virtual double? OilConsumption => null;
-    public virtual double? Gas2Consumption => null;
-    public virtual double? MaxElectricity => null;
-    public virtual string ImagePath => "avares://HeatProductionOptimization/Assets/ProductionUnits/no-image.png";
+    public virtual double? GasConsumption { get; set; } = null;
+    public virtual double? OilConsumption { get; set; } = null;
+    public virtual double? Gas2Consumption { get; set; } = null;
+    public virtual double? MaxElectricity { get; set; } = null;
+    public virtual string ImagePath { get; } = "avares://HeatProductionOptimization/Assets/ProductionUnits/no-image.png";
     public Bitmap Image => new Bitmap(AssetLoader.Open(new Uri(ImagePath)));
 
     public ProductionUnit(string ShortName, string Name, bool IsAvailable, double MaxHeat, decimal ProductionCosts,double? CO2Emissions )
@@ -44,75 +44,71 @@ public class ProductionUnit
 
 public class GasBoilersInfo : ProductionUnit
 {
-    private double _gasConsumption;
-    public override double? GasConsumption => _gasConsumption;
-    public override string ImagePath => "avares://HeatProductionOptimization/Assets/ProductionUnits/gas-boiler-image.png";
+    public override string ImagePath { get; } = "avares://HeatProductionOptimization/Assets/ProductionUnits/gas-boiler-image.png";
 
     public GasBoilersInfo(string ShortName, string Name, bool IsAvailable, double MaxHeat, decimal ProductionCosts, double CO2Emissions, double GasConsumption)
     : base(ShortName, Name, IsAvailable, MaxHeat, ProductionCosts, CO2Emissions)
     {
-        _gasConsumption = GasConsumption;
+        this.GasConsumption = GasConsumption;
     }
 }
 
 public class OilBoilerInfo : ProductionUnit
 {
-    private double _oilConsumption;
-    public override double? OilConsumption => _oilConsumption;
-    public override string ImagePath => "avares://HeatProductionOptimization/Assets/ProductionUnits/oil-boiler-image.png";
+    public override string ImagePath { get; } = "avares://HeatProductionOptimization/Assets/ProductionUnits/oil-boiler-image.png";
 
     public OilBoilerInfo(string ShortName, string Name, bool IsAvailable, double MaxHeat, decimal ProductionCosts, double CO2Emissions, double OilConsumption)
     : base(ShortName, Name, IsAvailable, MaxHeat, ProductionCosts, CO2Emissions)
     {
-        _oilConsumption = OilConsumption;
+        this.OilConsumption = OilConsumption;
     }
 }
 
 public class GasMotorInfo : ProductionUnit
 {
-    private double _gas2Consumption;
-    private double _maxElectricity;
-    
-    public override double? Gas2Consumption => _gas2Consumption;
-    public override double? MaxElectricity => _maxElectricity;
-    public override string ImagePath => "avares://HeatProductionOptimization/Assets/ProductionUnits/gas-motor-image.png";
+    public override string ImagePath { get; } = "avares://HeatProductionOptimization/Assets/ProductionUnits/gas-motor-image.png";
 
     public GasMotorInfo(string ShortName, string Name, bool IsAvailable, double MaxHeat, double MaxElectricity, decimal ProductionCosts, double CO2Emissions, double Gas2Consumption)
     : base(ShortName, Name, IsAvailable, MaxHeat, ProductionCosts, CO2Emissions)
     {
-        _gas2Consumption = Gas2Consumption;
-        _maxElectricity = MaxElectricity;
+        this.Gas2Consumption = Gas2Consumption;
+        this.MaxElectricity = MaxElectricity;
     }
 }
 
 public class ElectricBoilerInfo : ProductionUnit
 {
-    private double _maxElectricity;
-    public override double? MaxElectricity => _maxElectricity;
-    public override string ImagePath => "avares://HeatProductionOptimization/Assets/ProductionUnits/electric-boiler-image.png";
-    
+    public override string ImagePath { get; } = "avares://HeatProductionOptimization/Assets/ProductionUnits/electric-boiler-image.png";
+
     public ElectricBoilerInfo(string ShortName, string Name, bool IsAvailable, double MaxHeat, double MaxElectricity, decimal ProductionCost )
     : base(ShortName, Name, IsAvailable, MaxHeat, ProductionCost, null)
     {
-        _maxElectricity = MaxElectricity;
+        this.MaxElectricity = MaxElectricity;
     }
 }
 
 public class AssetManager
 {
+    private readonly UnitConfigurationService _configService;
 
-   public List<ProductionUnit> Units { get; } = new List<ProductionUnit>
+    public List<ProductionUnit> Units { get; }
+
+    public AssetManager()
     {
-        new GasBoilersInfo("GB1", "Gas Boiler 1", true, 3.0 , 510, 132, 1.05),
-        new GasBoilersInfo("GB2", "Gas Boiler 2", true, 2.0 , 540, 134, 1.08),
-        new GasBoilersInfo("GB3", "Gas Boiler 3", true, 4.0 , 580, 136, 1.09),
-        new OilBoilerInfo("OB1", "Oil Boiler 1", true, 6.0, 690, 147, 1.18),
-        new GasMotorInfo("GM1", "Gas Motor 1", true, 5.3, 3.9, 975, 227, 1.82),
-        new ElectricBoilerInfo("EB1", "Electric Boiler 1", true, 6.0, -6.0, 15)
-    };
+        _configService = new UnitConfigurationService();
+        Units = _configService.LoadUnits();
+    }
+
+    public void SaveUnits()
+    {
+        _configService.SaveUnits(Units);
+    }
 }
 
     
+
+
+
 
 
 
--- HeatProductionOptimization/ViewModels/AssetManagerViewModel.cs
+++ HeatProductionOptimization/ViewModels/AssetManagerViewModel.cs
@@ -1,54 +1,264 @@
 namespace HeatProductionOptimization.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Timers;
+using System.Windows.Input;
 using Avalonia.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using HeatProductionOptimization.Services;
 
 public class AssetManagerViewModel : ViewModelBase
 {
-	public List<ProductionUnitItemViewModel> Units { get; }
-    public AssetManager AssetManager { get; }
+	private bool _isEditMode;
+	private Timer _autoSaveTimer;
+	private bool _hasUnsavedChanges;
+
+	public ObservableCollection<ProductionUnitItemViewModel> Units { get; }
+	public AssetManager AssetManager { get; }
+	public ICommand AddNewUnitCommand { get; }
+	public ICommand DeleteUnitCommand { get; }
+
+	public bool IsEditMode
+	{
+		get => _isEditMode;
+		set => SetProperty(ref _isEditMode, value);
+	}
 
 	public AssetManagerViewModel(AssetManager? assetManager = null)
 	{
 		AssetManager = assetManager ?? new AssetManager();
-		Units = AssetManager.Units.Select(u => new ProductionUnitItemViewModel(u)).ToList();
+		Units = new ObservableCollection<ProductionUnitItemViewModel>(
+			AssetManager.Units.Select(u => new ProductionUnitItemViewModel(u, OnUnitChanged))
+		);
+
+		AddNewUnitCommand = new RelayCommand<string?>(type => { if (type != null) AddNewUnit(type); });
+		DeleteUnitCommand = new RelayCommand<ProductionUnitItemViewModel?>(unit => { if (unit != null) DeleteUnit(unit); });
+
+		_autoSaveTimer = new Timer(2000);
+		_autoSaveTimer.Elapsed += AutoSaveTimer_Elapsed;
+		_autoSaveTimer.AutoReset = false;
+	}
+
+	public void AddNewUnit(string type)
+	{
+		ProductionUnit? newUnit = type switch
+		{
+			"GasBoiler" => new GasBoilersInfo("GB_NEW", "New Gas Boiler", true, 1.0, 500, 100, 1.0),
+			"OilBoiler" => new OilBoilerInfo("OB_NEW", "New Oil Boiler", true, 1.0, 600, 110, 1.0),
+			"GasMotor" => new GasMotorInfo("GM_NEW", "New Gas Motor", true, 1.0, 1.0, 800, 150, 1.0),
+			"ElectricBoiler" => new ElectricBoilerInfo("EB_NEW", "New Electric Boiler", true, 1.0, 1.0, 50),
+			_ => null
+		};
+
+		if (newUnit != null)
+		{
+			AssetManager.Units.Add(newUnit);
+			Units.Add(new ProductionUnitItemViewModel(newUnit, OnUnitChanged));
+			TriggerAutoSave();
+		}
+	}
+
+	public void DeleteUnit(ProductionUnitItemViewModel unit)
+	{
+		var productionUnit = AssetManager.Units.FirstOrDefault(u => u.ShortName == unit.ShortName);
+		if (productionUnit != null)
+		{
+			AssetManager.Units.Remove(productionUnit);
+			Units.Remove(unit);
+			TriggerAutoSave();
+		}
+	}
+
+	private void OnUnitChanged()
+	{
+		_hasUnsavedChanges = true;
+		TriggerAutoSave();
+	}
+
+	private void TriggerAutoSave()
+	{
+		if (_autoSaveTimer.Enabled)
+		{
+			_autoSaveTimer.Stop();
+		}
+		_autoSaveTimer.Start();
+	}
+
+	private void AutoSaveTimer_Elapsed(object? sender, ElapsedEventArgs e)
+	{
+		if (_hasUnsavedChanges && IsEditMode)
+		{
+			AssetManager.SaveUnits();
+			_hasUnsavedChanges = false;
+		}
 	}
 }
 
 public class ProductionUnitItemViewModel : ObservableObject
 {
 	private readonly ProductionUnit unit;
+	private readonly Action _onChanged;
+	private string _shortName;
+	private string _name;
+	private double _maxHeat;
+	private decimal _productionCosts;
+	private double? _co2Emissions;
+	private double? _gasConsumption;
+	private double? _oilConsumption;
+	private double? _gas2Consumption;
+	private double? _maxElectricity;
+	private bool _isAvailable;
 
-	public string ShortName => unit.ShortName;
-	public string Name => unit.Name;
-	public double MaxHeat => unit.MaxHeat;
-	public decimal ProductionCosts => unit.ProductionCosts;
-	public double? CO2Emissions => unit.CO2Emissions;
-	public double? GasConsumption => unit.GasConsumption;
-	public double? OilConsumption => unit.OilConsumption;
-	public double? Gas2Consumption => unit.Gas2Consumption;
-	public double? MaxElectricity => unit.MaxElectricity;
-	public Bitmap Image => unit.Image;
-
-	public bool IsAvailable
+	public string ShortName
 	{
-		get => unit.IsAvailable;
+		get => _shortName;
 		set
 		{
-			if (unit.IsAvailable == value)
+			if (SetProperty(ref _shortName, value))
 			{
-				return;
+				unit.ShortName = value;
+				_onChanged?.Invoke();
 			}
-
-			unit.IsAvailable = value;
-			OnPropertyChanged(nameof(IsAvailable));
 		}
 	}
 
-	public ProductionUnitItemViewModel(ProductionUnit unit)
+	public string Name
+	{
+		get => _name;
+		set
+		{
+			if (SetProperty(ref _name, value))
+			{
+				unit.Name = value;
+				_onChanged?.Invoke();
+			}
+		}
+	}
+
+	public double MaxHeat
+	{
+		get => _maxHeat;
+		set
+		{
+			if (SetProperty(ref _maxHeat, value))
+			{
+				unit.MaxHeat = value;
+				_onChanged?.Invoke();
+			}
+		}
+	}
+
+	public decimal ProductionCosts
+	{
+		get => _productionCosts;
+		set
+		{
+			if (SetProperty(ref _productionCosts, value))
+			{
+				unit.ProductionCosts = value;
+				_onChanged?.Invoke();
+			}
+		}
+	}
+
+	public double? CO2Emissions
+	{
+		get => _co2Emissions;
+		set
+		{
+			if (SetProperty(ref _co2Emissions, value))
+			{
+				unit.CO2Emissions = value;
+				_onChanged?.Invoke();
+			}
+		}
+	}
+
+	public double? GasConsumption
+	{
+		get => _gasConsumption;
+		set
+		{
+			if (SetProperty(ref _gasConsumption, value))
+			{
+				unit.GasConsumption = value;
+				_onChanged?.Invoke();
+			}
+		}
+	}
+
+	public double? OilConsumption
+	{
+		get => _oilConsumption;
+		set
+		{
+			if (SetProperty(ref _oilConsumption, value))
+			{
+				unit.OilConsumption = value;
+				_onChanged?.Invoke();
+			}
+		}
+	}
+
+	public double? Gas2Consumption
+	{
+		get => _gas2Consumption;
+		set
+		{
+			if (SetProperty(ref _gas2Consumption, value))
+			{
+				unit.Gas2Consumption = value;
+				_onChanged?.Invoke();
+			}
+		}
+	}
+
+	public double? MaxElectricity
+	{
+		get => _maxElectricity;
+		set
+		{
+			if (SetProperty(ref _maxElectricity, value))
+			{
+				unit.MaxElectricity = value;
+				_onChanged?.Invoke();
+			}
+		}
+	}
+
+	public bool IsAvailable
+	{
+		get => _isAvailable;
+		set
+		{
+			if (SetProperty(ref _isAvailable, value))
+			{
+				unit.IsAvailable = value;
+				_onChanged?.Invoke();
+			}
+		}
+	}
+
+	public Bitmap Image => unit.Image;
+	public string UnitType => unit.GetType().Name;
+
+	public ProductionUnitItemViewModel(ProductionUnit unit, Action onChanged)
 	{
 		this.unit = unit ?? throw new ArgumentNullException(nameof(unit));
+		_onChanged = onChanged;
+
+		_shortName = unit.ShortName;
+		_name = unit.Name;
+		_maxHeat = unit.MaxHeat;
+		_productionCosts = unit.ProductionCosts;
+		_co2Emissions = unit.CO2Emissions;
+		_gasConsumption = unit.GasConsumption;
+		_oilConsumption = unit.OilConsumption;
+		_gas2Consumption = unit.Gas2Consumption;
+		_maxElectricity = unit.MaxElectricity;
+		_isAvailable = unit.IsAvailable;
 	}
 }
